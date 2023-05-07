b80bb322be6eb1164dcb991eb90c2ef9da742c0d
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-05-07 15:04:15 +0600

    web service

---
+++ MV/Services/Webservice.swift
@@ -0,0 +1,18 @@
+//
+//  Webservice.swift
+//  MV
+//
+//  Created by Shah Md Imran Hossain on 7/5/23.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case badURL
+    case badRequest
+    case decodingError
+}
+
+class Webservice {
+    
+}
