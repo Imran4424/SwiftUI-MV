51aa775267f6c6fd0bf8c3bbe046b9fff03ee222
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-05-07 15:08:14 +0600

    main actor

--- MV/Model/StoreModel.swift
+++ MV/Model/StoreModel.swift
@@ -7,6 +7,10 @@
 
 import Foundation
 
+// when you have a publised property of a class
+// then you need to make sure that class in the main thread
+// for that just add @MainActor before class delaration
+@MainActor
 class StoreModel: ObservableObject {
     @Published var products: [Product] = []
     
