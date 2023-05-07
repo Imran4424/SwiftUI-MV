0a69e9f45bfe091531638b21b0fb660e09ad3eaa
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-05-07 15:04:32 +0600

    implement web service

--- MV/Services/Webservice.swift
+++ MV/Services/Webservice.swift
@@ -14,5 +14,22 @@ enum NetworkError: Error {
 }
 
 class Webservice {
-    
+    func getProducts() async throws -> [Product] {
+        guard let url = URL(string: "https://fakestoreapi.com/products") else {
+            throw NetworkError.badURL
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let httpResponse = response as? HTTPURLResponse,
+              httpResponse.statusCode == 200 else {
+            throw NetworkError.badRequest
+        }
+        
+        guard let products = try? JSONDecoder().decode([Product].self, from: data) else {
+            throw NetworkError.decodingError
+        }
+        
+        return products
+    }
 }
